25502a8881c1f895cd3f3a716b8f419da9f139bb
Jarosław Orzeł <jaroslaw.orzel@emplocity.pl>
2021-12-20 22:29:02 +0100

    REST book table post

--- src/api/rest.py
+++ src/api/rest.py
@@ -1,8 +1,8 @@
 from typing import Any, Dict
 
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
 
-from models import Restaurant
+from models import Restaurant, User
 
 main = Blueprint("main", __name__)
 
@@ -21,3 +21,18 @@ def restaurants():
     session = current_app.session
     restaurants = [restaurant_serializer(r) for r in session.query(Restaurant)]
     return jsonify(restaurants)
+
+
+@main.route("/bookings", methods=["POST"])
+def bookings():
+    session = current_app.session
+    payload = request.get_json()
+    user_email = payload["user_email"]  # it should be done by token of course
+    restaurant_id = request.json["restaurant_id"]
+    persons = request.json["persons"]
+    user = session.query(User).filter_by(email=user_email).first()
+    restaurant = session.query(Restaurant).get(restaurant_id)
+    table_booking = restaurant.book_table(persons, user)
+    session.add(table_booking)
+    session.commit()
+    return jsonify({"isBooked": True}), 201
--- src/tests/test_rest_api.py
+++ src/tests/test_rest_api.py
@@ -1,5 +1,7 @@
 import pytest
 
+from models import TableBooking
+
 
 @pytest.fixture
 def test_client(app):
@@ -8,6 +10,7 @@ def test_client(app):
 
 def test_up_endpoint(test_client):
     response = test_client.get("/")
+
     assert response.status_code == 200
     assert response.json == {"up": True}
 
@@ -16,5 +19,30 @@ def test_get_restaurants(test_client, restaurant_factory, db_session):
     restaurant = restaurant_factory(name="taverna")
 
     response = test_client.get("/restaurants")
+
     assert response.status_code == 200
     assert response.json == [{"name": restaurant.name, "id": restaurant.id}]
+
+
+def test_post_bookings(
+    test_client, restaurant_factory, table_factory, user_factory, db_session
+):
+    restaurant = restaurant_factory(name="taverna")
+    _ = table_factory(restaurant=restaurant, max_persons=5, is_open=True)
+    user = user_factory(email="tester@example.pl")
+    persons = 3
+    payload = {
+        "restaurant_id": restaurant.id,
+        "persons": persons,
+        "user_email": user.email,
+    }
+
+    response = test_client.post("/bookings", json=payload)
+
+    assert response.status_code == 201
+    assert response.json == {"isBooked": True}
+    assert (
+        db_session.query(TableBooking)
+        .filter_by(user=user, restaurant=restaurant, persons=persons)
+        .first()
+    )
